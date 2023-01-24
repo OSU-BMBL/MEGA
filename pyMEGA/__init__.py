996dad436afe2f56bfe285077735c1a98336a4fb
Cankun Wang <flykun0620@gmail.com>
2023-01-24 02:17:24 -0500

    init pypi upload

--- pyMEGA/__init__.py
+++ pyMEGA/__init__.py
@@ -1,4 +1,18 @@
 """
 pyMEGA initialization module.
 """
+import logging
+from . import _version
 
+__version__ = _version.get_versions()["version"]
+
+from .log import create_logging_handler
+
+LOGGER = logging.getLogger(__name__)
+# Set logging level.
+LOGGING_DEBUG_OPT = False
+LOGGER.addHandler(create_logging_handler(LOGGING_DEBUG_OPT))
+LOGGER.setLevel(logging.DEBUG)
+
+from . import _version
+__version__ = _version.get_versions()['version']
